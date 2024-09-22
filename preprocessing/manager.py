371d74c6f07e26dbe3b4f9af2605ae5033c4d777
unknown <dolphin2410@outlook.kr>
2024-09-23 01:43:32 +0900

    fix CNN

--- preprocessing/manager.py
+++ preprocessing/manager.py
@@ -1,15 +1,15 @@
 import random
 
 import numpy as np
+from keras.src.utils import to_categorical
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv1D, Dense, Dropout
-from tensorflow.keras.layers import MaxPooling1D
+from tensorflow.keras.layers import Conv3D, Dense, Dropout
+from tensorflow.keras.layers import MaxPooling3D
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Masking
-from tensorflow.keras.optimizers import SGD
 
 from preprocessing import masking
 
@@ -23,7 +23,7 @@ class Manager:
         padded_data = masking.history_add_padding(self.history_data)
         masking_layer = masking.create_mask_layer(padded_data)
 
-        feature_size = masking_layer.shape[2] * masking_layer.shape[3]  # this case 51 - x, y, confidence for 17 parts
+        feature_size = masking_layer.shape[2] * (masking_layer.shape[3] - 1)  # this case 51 - x, y, confidence for 17 parts
         time_steps = masking_layer.shape[1]  # data per sequence
         batch_size = masking_layer.shape[0]
         print(masking_layer.shape)
@@ -38,10 +38,18 @@ class Manager:
             sequence_data = []  # This sequence data will be the timestep
             for time_step in history:
                 feature_list = []
+                std_first = -1
+                std_last = -1
                 for movenet_unit in time_step:
-                    feature_list.append(movenet_unit[0] + random.uniform(0.0, 3.0) - 1.5)  # Append x
-                    feature_list.append(movenet_unit[1] + random.uniform(0.0, 3.0) - 1.5)  # Append y
-                    feature_list.append(movenet_unit[2] + random.uniform(0.0, 3.0) - 1.5)  # Append z
+                    time_step = []
+                    if std_first == -1:
+                        std_first = movenet_unit[0]
+                    if std_last == -1:
+                        std_last = movenet_unit[1]
+                    time_step.append(movenet_unit[0])  # Append x
+                    time_step.append(movenet_unit[1])  # Append y
+                    time_step.append(movenet_unit[2])  # Append z
+                    feature_list.append(time_step)
                 sequence_data.append(feature_list)
             data_to_feed.append(sequence_data)
         return data_to_feed, self.answers
@@ -54,57 +62,75 @@ class Manager:
         data = np.array(data)
         answers = np.array(answers)
 
+        one_hot_labels = to_categorical(answers, num_classes=4)
+
+        num_sequences, num_frames, num_keypoints, features = data.shape
+        data = data.reshape((num_sequences, num_frames, num_keypoints, 1, features))
+
+        # import sys
+        # import numpy
+        # numpy.set_printoptions(threshold=sys.maxsize)
+        #
+        # print(data)
+
         # Scale data - Flatten and reshape due to dimension limit of StandardScaler
-        data_reshaped = data.reshape(-1, data.shape[2])
-        scaler = StandardScaler()
-        scaled_data_reshaped = scaler.fit_transform(data_reshaped)
-        scaled_data = scaled_data_reshaped.reshape(data.shape)
+        # data_reshaped = data.reshape(-1, data.shape[2])
+        # scaler = StandardScaler()
+        # scaled_data_reshaped = scaler.fit_transform(data_reshaped)
+        # scaled_data = scaled_data_reshaped.reshape(data.shape)
 
         # Train Test Split
-        train_x, val_x, train_y, val_y = train_test_split(np.array(scaled_data), np.array(answers), test_size=0.4, random_state=34)
+        train_x, val_x, train_y, val_y = train_test_split(np.array(data), np.array(one_hot_labels), test_size=0.2, random_state=34)
 
         model = Sequential()
 
         # Masking
-        model.add(Masking(0.0, input_shape=(time_steps, feature_size)))
+        # model.add(Masking(0.0, input_shape=(num_frames, num_keypoints, 1, features)))
 
         # Convolution Layers
-        model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(time_steps, feature_size)))
-        model.add(MaxPooling1D(pool_size=2))
-        model.add(Dropout(0.3))
-        model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
-        model.add(MaxPooling1D(pool_size=2, padding="same"))
-        model.add(Dropout(0.2))
+        model.add(Conv3D(32, kernel_size=(3, 3, 1), activation='relu', padding='same', input_shape=(num_frames, num_keypoints, 1, features)))
+        model.add(MaxPooling3D(pool_size=(2, 2, 1)))
+        model.add(Dropout(0.4))
 
         # Flatten for Fully Connected Layers
         model.add(Flatten())
 
         # Dense Layers
-        model.add(Dense(512, activation='sigmoid'))
-        model.add(Dense(256, activation='relu'))
-        model.add(Dense(128, activation='sigmoid'))
-        model.add(Dense(64, activation='relu'))
         model.add(Dense(32, activation='sigmoid'))
-        model.add(Dense(1, activation='relu'))
+        model.add(Dense(32, activation='relu'))
+        model.add(Dense(32, activation='sigmoid'))
+        model.add(Dense(32, activation='relu'))
+        model.add(Dense(32, activation='sigmoid'))
+        model.add(Dense(4, activation='softmax'))
 
-        model.compile(optimizer="adam", loss='mean_squared_error', metrics=['mae'])
+        model.compile(optimizer="adam", loss='categorical_crossentropy', metrics=['accuracy'])
         model.summary()
 
-        history = model.fit(train_x, train_y, epochs=3000, batch_size=10, validation_data=(val_x, val_y))
+        history = model.fit(train_x, train_y, epochs=200, batch_size=10, validation_data=(val_x, val_y))
 
         # Visualize actual results?
-        res = [round(float(model.predict(np.array([scaled_data[i]]))[0])) for i in range(len(answers))]
+        res = [model.predict(np.array([data[i]])) for i in range(len(answers))]
         print(res)
+        for i, x in enumerate(res):
+            x = x[0]
+            y = max(x[0], x[1], x[2], x[3])
+            if y == x[0]:
+                print(0 == answers[i])
+            elif y == x[1]:
+                print(1 == answers[i])
+            elif y == x[2]:
+                print(2 == answers[i])
+            else:
+                print(3 == answers[i])
+
 
         plt.figure(figsize=(12, 4))
 
-        plt.plot(history.history['loss'])
-        plt.plot(history.history['val_loss'])
-        plt.plot(history.history['mae'])
-        plt.plot(history.history['val_mae'])
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
         plt.title('Graph of Everything')
         plt.ylabel('Value')
         plt.xlabel('Epoch')
-        plt.legend(['TrainL', 'TestL', 'TrainA', 'TestA'], loc='upper left')
+        plt.legend(['TrainA', 'TestA'], loc='upper left')
         plt.tight_layout()
         plt.show()
